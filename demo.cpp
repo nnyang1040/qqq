4c06694f194dd1f07a9506e7e1e4057ebb503851
nny <1040333968@qq.com>
2022-05-31 20:51:49 +0800

    two

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+# 创建文件夹lusxGitRepositories
+mkdir lusxGitRepositories
+# 切换到lusxGitRepositories目录下    
+cd lusxGitRepositories       
+# 在当前目录新建一个Git代码库
+git init
+# 新建一个目录，将其初始化为Git代码库,c_practice为仓库的名称
+git init c_practice
+————————————————
+版权声明：本文为CSDN博主「超爱吃小蛋糕的66」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/guanjing_dream/article/details/122388401
